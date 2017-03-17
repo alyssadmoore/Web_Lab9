952c3b6d92f92dd6d73ae9b7471de7e4d727163d
alyssadmoore <alyssadmoore@gmail.com>
2017-03-16 19:37:11 -0500

    Change color of flowers viewed, click flower to view details page

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,38 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
-  req.db.collection('flowers').find().toArray(function(err, docs){
-    if (err) {
-      return next(err)
+  req.db.collection('flowers').distinct('color', function(err, colorDocs){
+    if (err){
+      return next(err);
     }
-    return res.render('all_flowers', {'flowers':docs});
+    if (req.query.color_filter){
+      req.db.collection('flowers').find({"color":req.query.color_filter}).toArray(function(err, docs){
+        if (err){
+          return next(err);
+        }
+        return res.render('all_flowers', {'flowers': docs, 'colors': colorDocs, 'color_filter': req.query.color_filter});
+      });
+
+    } else {
+      req.db.collection('flowers').find().toArray(function(err, docs){
+        if (err){
+          return next(err);
+        }
+        return res.render('all_flowers', {'flowers': docs, 'colors': colorDocs})
+      });
+    }
+  });
+});
+
+router.get('/details/:flower', function(req, res){
+  req.db.collection('flowers').findOne({'name' : req.params.flower}, function(err, doc){
+    if (err){
+      return next(err);
+    }
+    if (!doc){
+      return next();
+    }
+    return res.render('flower_details', {'flower' : doc});
   });
 });
 
